64b22a221e9b5be22fc726eee336cae8f8482ac4
c4710n <c4710n@users.noreply.github.com>
2024-05-25 11:00:31 +0800

    init: mix new ecto_i18n

---
+++ lib/ecto_i18n.ex
@@ -0,0 +1,18 @@
+defmodule EctoI18n do
+  @moduledoc """
+  Documentation for `EctoI18n`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoI18n.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_i18n_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoI18nTest do
+  use ExUnit.Case
+  doctest EctoI18n
+
+  test "greets the world" do
+    assert EctoI18n.hello() == :world
+  end
+end
