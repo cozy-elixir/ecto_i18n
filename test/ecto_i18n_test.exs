f85242413778d70e304c4b5b575f061596e751d0
c4710n <c4710n@users.noreply.github.com>
2024-05-25 11:05:45 +0800

    feat: prepare for publishing

--- test/ecto_i18n_test.exs
+++ test/ecto_i18n_test.exs
@@ -1,8 +1,8 @@
-defmodule EctoI18nTest do
+defmodule Ecto.I18nTest do
   use ExUnit.Case
-  doctest EctoI18n
+  doctest Ecto.I18n
 
   test "greets the world" do
-    assert EctoI18n.hello() == :world
+    assert Ecto.I18n.hello() == :world
   end
 end
