9f872e3dcc706d1c3f66a4125f51534a5eb7a83f
c4710n <c4710n@users.noreply.github.com>
2024-06-06 21:52:52 +0800

    fix: fix locales_without_parens

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,13 @@
 # Used by "mix format"
+locals_without_parens = [
+  field_i18n: 1,
+  field_i18n: 2,
+  field_i18n: 3
+]
+
 [
   import_deps: [:ecto, :ecto_sql],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [field_i18n: 1, field_i18n: 2, field_i18n: 3]
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
